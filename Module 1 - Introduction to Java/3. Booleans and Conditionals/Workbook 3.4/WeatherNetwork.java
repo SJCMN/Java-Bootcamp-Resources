b94ee360ab1590c410afa6794b86e56142e091e3
SJCMN <sam.clavette@gmail.com>
2022-01-31 12:01:20 -0600

    temp is freezing conditional

--- Module 1 - Introduction to Java/3. Booleans and Conditionals/Workbook 3.4/WeatherNetwork.java
+++ Module 1 - Introduction to Java/3. Booleans and Conditionals/Workbook 3.4/WeatherNetwork.java
@@ -1,15 +1,22 @@
 public class WeatherNetwork {
     public static void main(String[] args) {
 
-        int temp = 25;  
+        int temp = 12;  
 
         String forecast = "";
 
         //IF - ELSE IF - ELSE STATEMENTS HERE!
+        if ( temp < 20 ) {
+            System.out.println("The forecast is FREEZING! Stay home!");
+        } else if ( temp >= 20 && temp <= 32 ){
+            System.out.println("The forecast is Chilly. Wear a coat!");
+        } else {
+            System.out.println("It's warm. go outside!");
+        }
 
-        //if temp between -15 and -1: "The forecast is FREEZING! Stay home!"
+        //if temp between 0 and 20: "The forecast is FREEZING! Stay home!"
 
-        //if temp between 0 and 10: "The forecast is Chilly. Wear a coat!";
+        //if temp between 20 and 32: "The forecast is Chilly. Wear a coat!";
 
         //else: It's warm. go outside!
 
