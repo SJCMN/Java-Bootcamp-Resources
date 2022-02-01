bac217e36a976e457dc5a15d6300cf2f9a355c5d
SJCMN <sam.clavette@gmail.com>
2022-02-01 16:38:59 -0600

    adding workschedule

--- Module 1 - Introduction to Java/3. Booleans and Conditionals/Workbook 3.5/WorkSchedule.java
+++ Module 1 - Introduction to Java/3. Booleans and Conditionals/Workbook 3.5/WorkSchedule.java
@@ -1,8 +1,25 @@
 public class WorkSchedule {
     public static void main(String[] args) {
-        int day = 3;  
-        boolean holiday = true;
+        // Object weekDays = {
+        //     1: Sunday,
+        //     2: Monday,
+        //     3: Tuesday, 
+        //     4: Wednesday,
+        //     5: Thursday,
+        //     6: Friday,
+        //     7: Saturday
+        // }
+
+        int day = 7;  
+        boolean holiday = false;
          
+        if ( holiday ) {
+            System.out.println("woohoo, no work!");
+        } else if ( day == 1 || day == 7){
+            System.out.println("it's the weekend, no work!");
+        } else {
+            System.out.println("Wake up at 7:00 :(");
+        }
         // if it's a holiday, print: "woohoo, no work!");
         // if it's the weekend, print: "it's the weekend, no work!"
         // otherwise, print: "Wake up at 7:00 :( ";
