f64cdc67a3f5b993f09cc84acb2d02b4ed5a0a1b
SJCMN <sam.clavette@gmail.com>
2022-02-02 10:14:10 -0600

    add content

--- Module 1 - Introduction to Java/3. Booleans and Conditionals/Section 3/Dealership.java
+++ Module 1 - Introduction to Java/3. Booleans and Conditionals/Section 3/Dealership.java
@@ -10,9 +10,36 @@ public class Dealership {
         String option = scan.nextLine();
 
         switch (option) {
-            case "A": System.out.println("you chose " + option);
+            case "A": System.out.println("What is your budget?");
+                int budget = scan.nextInt();
+                if(budget >= 10000){
+                    System.out.println("Great! This Dodge Stratus is available");
+                    System.out.println("\nDo you have insurance? Enter 'Yes' or 'No'");
+                    scan.nextLine();
+                    String insurance = scan.nextLine();
+                    System.out.println("\nDo you have a drivers license? Enter 'Yes' or 'No'");
+                    String license = scan.nextLine();
+                    System.out.println("\nWhat is your credit score?");
+                    int creditScore = scan.nextInt();
+                    if ( insurance.equals("Yes") && license.equals("Yes") && creditScore > 660 ){
+                        System.out.println("Sold! Sign here and this car is yours!");
+                    } else {
+                        System.out.println("Get the F#&* outta here!");
+                    }
+                } else {
+                    System.out.println("Get outta here!");
+                }
                 break;
-            case "B": System.out.println("you chose " + option);
+            case "B": System.out.println("\nWhat is your car's value?");
+                int value = scan.nextInt();
+                System.out.println("\nWhat is your selling price?");
+                int price = scan.nextInt();
+                if ( value > price && price < 30000){
+                    System.out.println("\nWe'll buy that piece of s*(#!");
+                } else {
+                    System.out.println("\nNaa son, we're not interested");
+                }
+                
                 break;
             default: System.out.println("invalid option");
                 break;
